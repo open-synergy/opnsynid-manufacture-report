d12afc39fb249cb5d6ecbb1f0fa0d07ea3c1dea0
mikevhe18 <viriyananda.michael@gmail.com>
2022-04-19 08:29:30 +0700

    [opnsynid-manufacture-report] pre-commit fix

--- mrp_raw_material_calculation_report/reports/report_raw_material_calculation.py
+++ mrp_raw_material_calculation_report/reports/report_raw_material_calculation.py
@@ -6,15 +6,16 @@ from openerp.report import report_sxw
 
 
 class Parser(report_sxw.rml_parse):
-
     def __init__(self, cr, uid, name, context):
         super(Parser, self).__init__(cr, uid, name, context)
         self.lines = []
         self.temp_lines = []
         self.temp_dicts = {}
-        self.localcontext.update({
-            "line": self._get_rm_lines,
-        })
+        self.localcontext.update(
+            {
+                "line": self._get_rm_lines,
+            }
+        )
 
     def set_context(self, objects, data, ids, report_type=None):
         obj_wizard = self.pool.get("mrp.wizard_raw_material_calculation")
@@ -33,21 +34,24 @@ class Parser(report_sxw.rml_parse):
                 detail.bom_id.product_uom.id,
             )
             for bom_line in obj_bom._bom_explode(
-                    self.cr,
-                    self.uid,
-                    detail.bom_id,
-                    detail.product_id, factor / detail.bom_id.product_qty)[0]:
+                self.cr,
+                self.uid,
+                detail.bom_id,
+                detail.product_id,
+                factor / detail.bom_id.product_qty,
+            )[0]:
                 uom_id = bom_line["product_uom"]
                 uom = obj_uom.browse(self.cr, self.uid, [uom_id])[0]
                 bom_line.update({"uom": uom.name})
                 self.temp_lines.append(bom_line)
 
         for temp_line in self.temp_lines:
-            if not self.temp_dicts.has_key(temp_line["product_id"]):
+            if "product_id" in temp_line:
                 self.temp_dicts[temp_line["product_id"]] = temp_line
             else:
-                self.temp_dicts[temp_line["product_id"]]["product_qty"] += \
-                    temp_line["product_qty"]
+                self.temp_dicts[temp_line["product_id"]]["product_qty"] += temp_line[
+                    "product_qty"
+                ]
 
         for temp_dict in self.temp_dicts.values():
             self.lines.append(temp_dict)
